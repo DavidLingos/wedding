661283444f95986ab90196bf448000f6e9d7eb0e
david.ling <david.ling@notino.com>
2023-07-19 17:16:35 +0200

    feat: remove date

--- lavelo/src/components/rsvp/index.js
+++ lavelo/src/components/rsvp/index.js
@@ -11,7 +11,6 @@ const Rsvp = () => {
                     <div className="col-12">
                         <div className="section-title section-title4 text-center">
                             <h2>Potvrzení účasti</h2>
-                            <p>Prosím, potvrďte nám svou účast do 15. května.</p>
                         </div>
                     </div>
                     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZwwUduu00ZjqfZhtV13ohQs0EyOrjX_7TCvBzwgUnEZ7y7g/viewform?embedded=true" width="640" height="1475" frameborder="0" marginheight="0" marginwidth="0">Načítání…</iframe>;
